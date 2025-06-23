5d6e56c13c7d1cbcae66f35f8cf7303c65be2a6a
CbumDaddy <pedro_alencar@ufms.br>
2025-06-23 01:19:18 -0300

    fix: ajuste em DATABASE no settings

--- speedgarage/settings.py
+++ speedgarage/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-dev-key")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.getenv(
-    "DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1"
+    "DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1",""
 ).split(",")
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "DJANGO_CSRF_TRUSTED_ORIGINS",
