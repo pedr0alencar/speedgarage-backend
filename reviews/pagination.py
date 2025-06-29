ff29954b62c1a05059882da95b9c11575c4142ec
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-06-29 17:49:54 -0400

    feat: adiciona 50 no default da paginaçao

--- reviews/pagination.py
+++ reviews/pagination.py
@@ -2,5 +2,5 @@
 from rest_framework.pagination import LimitOffsetPagination
 
 class DefaultLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 5      # 5 objetos por página
+    default_limit = 50      # 50 objetos por página
     max_limit = 50         # protege contra consultas gigantes
